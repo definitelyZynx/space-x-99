ff1ff8409bd89fe2d666aebb4f999437cc0dc9c5
Jhoemer Muyco <muyco.jhoemer1@gmail.com>
2024-04-06 17:09:31 +0800

    Added logos.

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,8 @@ function App() {
   }, [hasNextPage, !isLoading]);
 
   return (
-    <div className="px-2 md:px-52 py-8 w-full min-h-full bg-slate-100">
+    <div className="px-2 md:px-52 py-8 w-full min-h-full bg-slate-100 flex flex-col items-center">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/SpaceX_logo_black.svg" className="ml-10 mb-5 w-[300px]" />
       <div className="w-full min-h-full p-5">
         <input
           className="w-full border border-slate-300 py-2 px-3 outline-none rounded-[4px]"
